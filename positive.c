5f35eaeb4b4878deebd8c199a66afc5bdcda36df
lavanyapavi <lavanyav2979@gmail.com>
2017-06-05 11:57:09 +0530

    Add files via upload

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the value");
+scanf("%d",&a);
+if(a>0)
+printf("The value is positive");
+else if(a<0)
+printf("The value is negative");
+else
+printf("The value is zero");
+}
